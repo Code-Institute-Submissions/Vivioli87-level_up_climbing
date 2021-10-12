47d417db63d273e31a2b919d782b7c5667a2cfb1
Vivioli87 <vivnightingale@hotmail.com>
2021-10-12 23:42:07 +0000

    created checkout app for bookings. updated models admin and forms

---
+++ checkout/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Booking
+
+
+class BookingAdmin(admin.ModelAdmin):
+    readonly_fields = ('booking_reference', 'date',
+                       'booking_total', 'booked_course',)
+
+    fields = ('booking_reference', 'booked_course', 'date', 
+              'full_name', 'email', 'phone_number',
+              'booking_total',)
+
+    list_display = ('booking_reference', 'booked_course', 'date', 
+                    'full_name', 'email', 'phone_number',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Booking, BookingAdmin)
---
+++ checkout/models.py
@@ -0,0 +1,35 @@
+import uuid
+
+from django.db import models
+
+from courses.models import Course
+
+class Booking(models.Model):
+
+    booking_reference = models.CharField(max_length=32, null=False,
+                                         editable=False)
+    full_name = models.CharField(max_length=60, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    booking_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                        null=False, default=0)
+    booked_course = models.ForeignKey(Course, null=False, blank=False, on_delete=models.CASCADE)
+
+    def _create_booking_reference(self):
+        """
+        Generate a random, unique booking reference using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the booking refernce
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._create_booking_reference()
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.booking_reference
