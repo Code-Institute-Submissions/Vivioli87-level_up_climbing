786975d7dac27937ebceefb7797708c4da190bb6
Vivioli87 <vivnightingale@hotmail.com>
2021-10-18 21:13:42 +0000

    Set up webhook handlers and tested on Stripe

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
+
 
 urlpatterns = [
     path('<int:course_id>/', views.booking, name='booking'),
-    path('booking_success/<booking_reference>/', views.booking_success, name='booking_success'),
+    path('booking_success/<booking_reference>/', views.booking_success,
+         name='booking_success'),
+    path('wh/', webhook, name='webhook'),
 ]
