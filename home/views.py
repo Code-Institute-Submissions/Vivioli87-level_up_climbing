1b99f8a0715d2fc1d3f9f38374426fa09632dba1
Vivioli87 <vivnightingale@hotmail.com>
2021-10-22 23:10:12 +0000

    added venue overview to about page and linked venue.html

--- home/views.py
+++ home/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
+from venues.models import Venue
+
 
 def index(request):
     return render(request, 'home/index.html')
 
 
 def about(request):
-    return render(request, 'home/about.html')
+
+    venues = Venue.objects.all()
+
+    context = {
+        'venues': venues,
+    }
+
+    return render(request, 'home/about.html', context)
