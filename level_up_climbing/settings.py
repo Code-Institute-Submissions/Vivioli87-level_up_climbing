ad87fd25c1d352ef4d30b881ef7608bf66eb8545
Vivioli87 <vivnightingale@hotmail.com>
2021-10-25 22:31:56 +0000

    added Procfile and added heroku app to allowed hosts

--- level_up_climbing/settings.py
+++ level_up_climbing/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-+4naxx01uy9cv@1_#(79%!b*42*+^yn1revi74#2u37^2&7go6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['level-up-climbing.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,7 +123,12 @@ WSGI_APPLICATION = 'level_up_climbing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
