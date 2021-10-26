1473baa86376abf9898292e4eb42e61663f510ac
Vivioli87 <vivnightingale@hotmail.com>
2021-10-26 02:41:28 +0000

    added content and styled profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import UserProfile
+from .models import UserProfile, Coach
 from checkout.models import Booking
 from courses.models import Course
+
+from .forms import UserInfoForm
 
 
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
     bookings = Booking.objects.filter(user_profile=profile)
+    coach = Coach.objects.all()
     course = Course.objects.all()
 
     template = 'profiles/profile.html'
@@ -16,6 +19,7 @@ def profile(request):
         'profile': profile,
         'bookings': bookings,
         'course': course,
+        'coach': coach,
     }
 
     return render(request, template, context)
