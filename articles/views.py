e7c1eca3aefa273bac70d12d06f2666866b418db
Vivioli87 <vivnightingale@hotmail.com>
2021-10-28 20:14:10 +0000

    added article detail page, views, styling etc

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Article
 
@@ -10,6 +10,18 @@ def all_articles(request):
     template = 'articles/all_articles.html'
     context = {
         'articles': articles,
+    }
+
+    return render(request, template, context)
+
+
+def article_detail(request, article_id):
+
+    article = get_object_or_404(Article, pk=article_id)
+
+    template = 'articles/article_detail.html'
+    context = {
+        'article': article,
     }
 
     return render(request, template, context)
