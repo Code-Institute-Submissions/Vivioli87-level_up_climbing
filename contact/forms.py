77c3bc22cb0573565a796dc45c14d4e76c115811
Vivioli87 <vivnightingale@hotmail.com>
2021-10-30 15:09:26 +0000

    added 1:1 coaching contact forms, views, urls and templates

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-from .models import GeneralContact
+from .models import GeneralContact, PrivateCoachingContact
+
+from venues.models import Venue
+from courses.models import Level
 
 
 
@@ -40,3 +43,46 @@ class CompleteContactForm(forms.ModelForm):
         for field in self.fields:
             if field != 'is_complete':
                 self.fields[field].disabled = True
+
+
+class PrivateCoachingForm(forms.ModelForm):
+    class Meta:
+        model = PrivateCoachingContact
+        exclude = ('date_sent', 'is_complete')
+    
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email',
+            'phone_number': 'Phone number',
+            'message': 'Message',
+            'level': 'Level',
+            'venue': 'Venue',
+        }
+
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0'
+            self.fields['level'] = forms.ModelChoiceField(queryset=Level.objects.all(), label='Current Level')
+            self.fields['venue'] = forms.ModelChoiceField(queryset=Venue.objects.all(), label='Preferred Venue')
+            self.fields[field].label = False
+        
+
+
+class CompletePrivateCoachingForm(forms.ModelForm):
+    class Meta:
+        model = PrivateCoachingContact
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+ 
+        self.fields['is_complete'] = forms.BooleanField(required=False, initial=False, label='Mark as complete?')
+        for field in self.fields:
+            if field != 'is_complete':
+                self.fields[field].disabled = True
+
--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 
-from .forms import ContactForm, CompleteContactForm
-from .models import GeneralContact
+from .forms import ContactForm, CompleteContactForm, PrivateCoachingForm, CompletePrivateCoachingForm
+from .models import GeneralContact, PrivateCoachingContact
+
 
 def contact_form(request):
     if request.method == 'POST':
@@ -27,14 +28,39 @@ def contact_form(request):
     return render(request, template, context)
 
 
+def coaching_contact_form(request):
+    if request.method == 'POST':
+        form = PrivateCoachingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully submitted 1:1 coaching enquiry!')
+            return redirect('home')
+        else:
+            messages.error(request,
+                           ('Failed to submit enquiry. '
+                            'Please ensure the form is valid.'))
+
+    else:
+        form = PrivateCoachingForm()
+
+    template = 'contact/coaching_contact.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
 def contact_manager(request):
 
     general_contacts = GeneralContact.objects.filter(is_complete=False).order_by('-date_sent')
+    coaching_contacts = PrivateCoachingContact.objects.filter(is_complete=False).order_by('-date_sent')
     
     template = 'contact/contact_manager.html'
 
     context = {
         'general_contacts': general_contacts,
+        'coaching_contacts': coaching_contacts,
     }
 
     return render(request, template, context)
@@ -51,13 +77,40 @@ def contact_detail(request, contact_id):
             return redirect(reverse('contact_manager'))
         else:
             messages.error(request,
-                           ('Failed to marke enquiry as complete. '
+                           ('Failed to mark enquiry as complete. '
                             'Please ensure the form is valid.'))
 
     else:
         form = CompleteContactForm(instance=contact)
 
     template = 'contact/contact_detail.html'
+
+    context = {
+        'form': form,
+        'contact': contact,
+    }
+
+    return render(request, template, context)
+
+
+def coaching_contact_detail(request, contact_id):
+    contact = get_object_or_404(PrivateCoachingContact, pk=contact_id)
+
+    if request.method == 'POST':
+        form = CompletePrivateCoachingForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully completed coaching enquiry!')
+            return redirect(reverse('contact_manager'))
+        else:
+            messages.error(request,
+                           ('Failed to mark enquiry as complete. '
+                            'Please ensure the form is valid.'))
+
+    else:
+        form = CompletePrivateCoachingForm(instance=contact)
+
+    template = 'contact/coaching_contact_detail.html'
 
     context = {
         'form': form,
